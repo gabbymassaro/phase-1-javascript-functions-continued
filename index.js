d23698a5d4f338a94b44bddf9dadf27fbe6a0199
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-06 22:10:22 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(star = '*') {
+    innerFunction = function (adjective = "special"){
+        return `You are ${star}${adjective}${star}!`;
+    }
+    return innerFunction;
+}
